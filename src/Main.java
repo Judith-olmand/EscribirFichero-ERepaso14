4e94113558ecc2b615fd324a60642f35f924ce5c
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-05-20 12:07:55 +0200

    acabado

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el nombre del archivo");
+        String archivo = sc.nextLine();
+
+        System.out.println("Escribe 5 nombres:");
+
+        for (int i = 1; i <= 5; i++) {
+            String nombre = sc.nextLine();
+            //FILE WRITER
+            try (FileWriter writer = new FileWriter(archivo, true)) {
+                writer.write(nombre + "\n");
+            } catch (IOException e) {
+                System.out.println("Ocurrió un error al escribir en el archivo: " + e.getMessage());
+            }
+
+        }
+    }
 }
